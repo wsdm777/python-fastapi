4762d4a8df359f0e41f5e595d3c85db8ddda3349
wsdm <kuznetsov06denis@yandex.ru>
2025-01-15 21:01:30 +0300

    add login logs

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import Depends, Request
+from fastapi import Depends, Request, Response
 from fastapi_users import BaseUserManager, IntegerIDMixin
 from src.databasemodels import User
 from src.database import get_user_db
@@ -10,7 +10,15 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     async def on_after_register(
         self, user: User, request: Optional[Request] = None
     ) -> None:
-        logger.info(f"User {user.id} registered")
+        logger.info(f"User {user.email} registered")
+
+    async def on_after_login(
+        self,
+        user: User,
+        request: Optional[Request] = None,
+        response: Optional[Response] = None,
+    ) -> None:
+        logger.info(f"User {user.email} login")
 
 
 async def get_user_manager(user_db=Depends(get_user_db)):
