841f517dbb91a05bf5689fc25ab76e66b47500de
wsdm <kuznetsov06denis@yandex.ru>
2025-02-11 02:39:52 +0300

    add test for section router

---
+++ tests/test_section.py
@@ -0,0 +1,95 @@
+from fastapi import status
+import pytest
+
+base = "/section/"
+
+
+@pytest.mark.parametrize(
+    "client_fixture, expected_status",
+    [
+        ("admin_client", status.HTTP_201_CREATED),
+        ("regular_client", status.HTTP_403_FORBIDDEN),
+        ("unauthorized_client", status.HTTP_401_UNAUTHORIZED),
+    ],
+    indirect=["client_fixture"],
+)
+async def test_section_add(client_fixture, expected_status):
+    respond = await client_fixture.post(
+        base + "add",
+        json={"name": "Section"},
+    )
+    assert respond.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    "client_fixture, expected_status",
+    [
+        ("regular_client", status.HTTP_200_OK),
+        ("unauthorized_client", status.HTTP_401_UNAUTHORIZED),
+    ],
+    indirect=["client_fixture"],
+)
+async def test_get_section(client_fixture, expected_status):
+    respond = await client_fixture.get(base + "Section")
+    assert respond.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    "client_fixture, expected_status",
+    [
+        ("admin_client", status.HTTP_200_OK),
+        ("regular_client", status.HTTP_403_FORBIDDEN),
+        ("unauthorized_client", status.HTTP_401_UNAUTHORIZED),
+    ],
+    indirect=["client_fixture"],
+)
+async def test_put_section(client_fixture, expected_status):
+    respond = await client_fixture.put(
+        base + "update",
+        json={"name": "Section", "head_id": 1},
+    )
+    assert respond.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    "client_fixture, expected_status",
+    [
+        ("regular_client", status.HTTP_200_OK),
+        ("unauthorized_client", status.HTTP_401_UNAUTHORIZED),
+    ],
+    indirect=["client_fixture"],
+)
+@pytest.mark.parametrize(
+    "params",
+    [
+        {},
+        {"desc": True},
+        {"desc": False},
+        {"filter_name": "Отдел разработки"},
+        {"page_size": 1},
+        {"page_size": 100},
+        {"last_section_name": "Отдел тестирования"},
+        {"desc": True, "page_size": 50, "filter_name": "Отдел аналитики"},
+        {"desc": False, "page_size": 10, "last_section_name": "Отдел маркетинга"},
+    ],
+)
+async def test_get_sections(client_fixture, expected_status, params):
+    respond = await client_fixture.get(base + "list/", params=params)
+    assert respond.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    "client_fixture, expected_status",
+    [
+        ("admin_client", status.HTTP_200_OK),
+        ("regular_client", status.HTTP_403_FORBIDDEN),
+        ("unauthorized_client", status.HTTP_401_UNAUTHORIZED),
+    ],
+    indirect=["client_fixture"],
+)
+async def test_delete_section(client_fixture, expected_status):
+    respond = await client_fixture.delete(base + "delete/Section")
+    assert respond.status_code == expected_status
+    if respond.status_code == status.HTTP_200_OK:
+        respond = await client_fixture.get(base + "Section")
+        assert respond.status_code == status.HTTP_404_NOT_FOUND
